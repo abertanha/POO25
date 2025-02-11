cd3fcb433c6d423e8beb6772c05f952fee5cf5eb
abertanha <adriano2604@hotmail.com>
2025-02-11 11:00:28 -0300

    Primeiro exemplo de java

---
+++ prjExemplo1/src/Exemplo1.java
@@ -0,0 +1,21 @@
+/**
+ * @author 0030482321026 Adriano Bertanha
+ */
+public class Exemplo1 {
+    public static void main(String[] args) {
+        int idade;
+        double peso;
+        char sexo;
+        String nome;
+        
+        idade = 34;
+        peso = 78.5;
+        sexo = 'F';
+        nome = "Ana Beatriz";
+        
+        System.out.println("idade: " + idade);
+        System.out.println("peso: " + peso);
+        System.out.println("sexo: " + sexo);
+        System.out.println("nome: " + nome);
+    }
+}
