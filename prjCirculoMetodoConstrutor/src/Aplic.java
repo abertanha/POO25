35a95569b4048fa80e0ca58d2bf599214835d3b3
abertanha <adriano2604@hotmail.com>
2025-03-10 08:05:41 -0300

    Correcao exercicio circulo

--- prjCirculoMetodoConstrutor/src/Aplic.java
+++ prjCirculoMetodoConstrutor/src/Aplic.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import fatec.poo.model.Circulo;
+import java.text.DecimalFormat;
 /**
  *
  * @author shenmue
@@ -8,14 +9,26 @@ public class Aplic {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        DecimalFormat number = new DecimalFormat("#.00");
         String unidadeMedida;
+        double raio;
         
         System.out.print("Entre a unidade de medida do raio: ");
         unidadeMedida = entrada.nextLine();
         
+        System.out.print("Entre a unidade de medida do raio: ");
+        raio = entrada.nextDouble();
+        
+        
         Circulo objCirc = new Circulo(unidadeMedida);
+        objCirc.setRaio(raio);
+        double areaCirc = objCirc.calcArea();
+        double periCirc = objCirc.calcPerimetro();
         
         System.out.println("A unidade de medida do raio é: "+ objCirc.getUnidadeMedida());
+        System.out.println("O raio do circulo é: "+ objCirc.getRaio() + objCirc.getUnidadeMedida());
+        System.out.println("A area do circulo é: "+ number.format(areaCirc));
+        System.out.println("O perimetro do circulo é: "+ number.format(periCirc) + objCirc.getUnidadeMedida());        
     }
     
 }
--- prjCirculoMetodoConstrutor/src/fatec/poo/model/Circulo.java
+++ prjCirculoMetodoConstrutor/src/fatec/poo/model/Circulo.java
@@ -7,11 +7,24 @@ package fatec.poo.model;
  */
 public class Circulo {
     private String unidadeMedida;
+    private double raio;
     
     public Circulo(String unidadeMedida){
         this.unidadeMedida = unidadeMedida;
     }
     public String getUnidadeMedida(){
         return this.unidadeMedida;
+    }
+    public void setRaio(double raio){
+        this.raio = raio;
+    }
+    public double getRaio(){
+        return this.raio;
+    }
+    public double calcArea(){
+        return (Math.pow(this.raio, 2)*Math.PI);
+    }
+    public double calcPerimetro(){
+        return (Math.PI*this.raio*2);
     }
 }
