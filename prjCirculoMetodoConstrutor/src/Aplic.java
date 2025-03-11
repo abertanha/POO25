7702c0f8f01e8643cbd6ac44f8d09d2e5cd9b8f4
abertanha <adriano2604@hotmail.com>
2025-03-11 09:43:57 -0300

    :refactor:Retirado variáveis desnecessárias

--- prjCirculoMetodoConstrutor/src/Aplic.java
+++ prjCirculoMetodoConstrutor/src/Aplic.java
@@ -14,21 +14,15 @@ public class Aplic {
         double raio;
         
         System.out.print("Entre a unidade de medida do raio: ");
-        unidadeMedida = entrada.nextLine();
+        Circulo objCirc = new Circulo(entrada.nextLine());
         
         System.out.print("Entre a unidade de medida do raio: ");
-        raio = entrada.nextDouble();
-        
-        
-        Circulo objCirc = new Circulo(unidadeMedida);
-        objCirc.setRaio(raio);
-        double areaCirc = objCirc.calcArea();
-        double periCirc = objCirc.calcPerimetro();
+        objCirc.setRaio(entrada.nextDouble());
         
         System.out.println("A unidade de medida do raio é: "+ objCirc.getUnidadeMedida());
         System.out.println("O raio do circulo é: "+ objCirc.getRaio() + objCirc.getUnidadeMedida());
-        System.out.println("A area do circulo é: "+ number.format(areaCirc));
-        System.out.println("O perimetro do circulo é: "+ number.format(periCirc) + objCirc.getUnidadeMedida());        
+        System.out.println("A area do circulo é: "+ number.format(objCirc.calcArea())+objCirc.getUnidadeMedida()+ "²");
+        System.out.println("O perimetro do circulo é: "+ number.format(objCirc.calcPerimetro()) + objCirc.getUnidadeMedida());        
     }
     
 }
