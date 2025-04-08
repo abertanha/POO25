a728b656a48802290b81138f4b6a1a93d05d9b23
abertanha <adriano2604@hotmail.com>
2025-04-08 11:08:13 -0300

    Primeira atividade de Associação Binária

--- prjAssocBinPessoaFisicaJuridica/src/Aplic.java
+++ prjAssocBinPessoaFisicaJuridica/src/Aplic.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.text.DecimalFormat;
 import fatec.poo.model.*;
+import java.util.Date;
 
 /**
  * @author 0030482321026
@@ -8,45 +9,65 @@ import fatec.poo.model.*;
 public class Aplic {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        DecimalFormat number = new DecimalFormat("#.00");
+        DecimalFormat number = new DecimalFormat("#.###,00");
+        Date data = new Date();
         
-        PessoaFisica objPeFis = new PessoaFisica("11122233344",
-                                                "Clotilde Ferreira",
-                                                2012);
-        PessoaJuridica objPeJuri = new PessoaJuridica("1112223/55555",
-                                                      "Mecânica Arvore Grande",
-                                                      1987);
+        System.out.println("Entre os dados da Pessoa Física: ");
+        System.out.println("CPF: ");
+        String cpf = entrada.next();
+        System.out.println("Nome: ");
+        String nome = entrada.next();
+        System.out.println("Ano de inscrição: ");
+        int anoInscricao = entrada.nextInt();
+        
+        PessoaFisica objPeFis = new PessoaFisica(cpf, nome, anoInscricao);
+        
+        System.out.println("\n\nEntre os dados da Pessoa Jurídica: ");
+        System.out.println("CGC: ");
+        String cgc = entrada.next();
+        System.out.println("Nome: ");
+        String nomePj = entrada.next();
+        System.out.println("Ano de inscrição: ");
+        int anoInscricaoPj = entrada.nextInt();
+        
+        PessoaJuridica objPeJuri = new PessoaJuridica(cgc, nomePj, anoInscricaoPj);
+        
+        //definição da matriz objetos da classe PedidoCompra
         PedidoCompra[] cadPedidos = new PedidoCompra[10];
         
-        
-        boolean sessao = true;
-        while(sessao){
-            System.out.println("\n1 - Adicionar compras\n2 - Informar Base\n3 - Sair\n\tDigite uma opção");
-            int opcao = entrada.nextInt();
-            switch (opcao){
-                case 1:
-                    boolean compras = true;
-                    while(compras){
-                        System.out.println("Insira o valor da compra ou insira zero se quiser parar: ");
-                        double valor = entrada.nextDouble();
-                        if(valor != 0){
-                            objPeFis.addCompras(valor);
-                        }else {
-                            compras = false;
-                        }
-                    }
-                    break;
-                case 2:
-                    System.out.println("Insira o valor de base: ");
-                    objPeFis.setBase(entrada.nextDouble());
-                    break;
-                case 3:
-                    sessao = false;
-                    break;
-                default:
-                    System.out.println("Operação inválida.");
+        int cont = 0;
+        while(cont < 10){
+            System.out.println("Insira o numero do pedido: ");
+            cadPedidos[cont] = new PedidoCompra(entrada.nextInt());
+            System.out.println("Insira a data do pedido: ");
+            cadPedidos[cont].setDataPedido(entrada.next());
+            System.out.println("Insira o valor do pedido: ");
+            cadPedidos[cont].setValor(entrada.nextFloat());
+            
+            boolean sessao = true;
+            while(sessao){
+                System.out.println("\n1 - Pessoa Física\n2 - Pessoa Jurídica\n\tDigite uma opção");
+                int opcao = entrada.nextInt();
+                switch (opcao){
+                    case 1:
+                        System.out.println("\nContabilizado para Pessoa Física");
+                        cadPedidos[cont].setPessoa(objPeFis);
+                        objPeFis.addPedidoCompra(cadPedidos[cont++]);
+                        sessao = false;
+                        break;
+                    case 2:
+                        System.out.println("\nContabilizado para Pessoa Jurídica");
+                        cadPedidos[cont].setPessoa(objPeJuri);
+                        objPeJuri.addPedidoCompra(cadPedidos[cont++]);
+                        sessao = false;
+                        break;
+                    default:
+                        System.out.println("Operação inválida.");
+                }
             }
         }
+        
+        System.out.println("Data atual: "+ data);
         System.out.println("\n\nPessoa Física");
         System.out.println("Nome                => " + objPeFis.getNome());
         System.out.println("Ano de inscrição    => " + objPeFis.getAnoInscricao());
@@ -54,39 +75,7 @@ public class Aplic {
         System.out.println("CPF                 => " + objPeFis.getCpf());
         System.out.println("Base                => " + objPeFis.getBase());
         System.out.println("Bonus               => " + objPeFis.calcBonus(2025));
-        
-        
-        
-        
-        
-        sessao = true;
-        while(sessao){
-            System.out.println("\n1 - Adicionar compras\n2 - Informar Taxa de Incentivo\n3 - Sair\n\tDigite uma opção");
-            int opcao = entrada.nextInt();
-            switch (opcao){
-                case 1:
-                    boolean compras = true;
-                    while(compras){
-                        System.out.println("Insira o valor da compra ou insira zero se quiser parar: ");
-                        double valor = entrada.nextDouble();
-                        if(valor != 0){
-                            objPeJuri.addCompras(valor);
-                        }else {
-                            compras = false;
-                        }
-                    }
-                    break;
-                case 2:
-                    System.out.println("Insira o valor de taxa de incentivo: ");
-                    objPeJuri.setTaxaIncentivo(entrada.nextDouble());
-                    break;
-                case 3:
-                    sessao = false;
-                    break;
-                default:
-                    System.out.println("Operação inválida.");
-            }
-        }
+       
         System.out.println("Pessoa Jurídica");
         System.out.println("Nome                => " + objPeJuri.getNome());
         System.out.println("Ano de inscrição    => " + objPeJuri.getAnoInscricao());
@@ -94,6 +83,5 @@ public class Aplic {
         System.out.println("CPF                 => " + objPeJuri.getCgc());
         System.out.println("Taxa de incentivo   => " + objPeJuri.getTaxaIncentivo() + "%");
         System.out.println("Bonus               => " + objPeJuri.calcBonus(2025));
-    }
-    
+    } 
 }
--- prjAssocBinPessoaFisicaJuridica/src/fatec/poo/model/Pessoa.java
+++ prjAssocBinPessoaFisicaJuridica/src/fatec/poo/model/Pessoa.java
@@ -7,29 +7,29 @@ abstract public class Pessoa {
     private String nome;
     private int anoInscricao;
     private double totalCompras;
-    private PedidoCompra[] pedidos;
+    private PedidoCompra[] pedidos; //multiplicidade 1..*
     private int numPed;
     
     public Pessoa(String nome, int anoInscricao){
         this.nome = nome;
         this.anoInscricao = anoInscricao;
         pedidos = new PedidoCompra[10];
-        numPed = 0;
-        
+        numPed = 0;        
     }
     abstract public double calcBonus(int anoAtual);
     
-    public void addCompras(){
+    public void addPedidoCompra(PedidoCompra pedido){
+        pedidos[numPed] = pedido;
         totalCompras += pedidos[numPed++].getValor();
     }
     public String getNome(){
-        return this.nome;
+        return nome;
     }
     public int getAnoInscricao(){
-        return this.anoInscricao;
+        return anoInscricao;
     }
     public double getTotalCompras(){
-        return this.totalCompras;
+        return totalCompras;
     }
     public void setNome(String nome) {
         this.nome = nome;
